6b3fc6f6986a7103750468bc27de4f1c39e12241
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-22 22:19:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+document.getElementById("change_button").addEventListener("click", function () {
+            document.querySelectorAll(".grid-item").forEach(item => {
+                item.style.backgroundColor = "transparent";
+            });
+
+            let blockId = document.getElementById("block_id").value;
+            let color = document.getElementById("colour_id").value;
+
+            if (blockId >= 1 && blockId <= 9 && color) {
+                document.getElementById(blockId).style.backgroundColor = color;
+            } else {
+                alert("Enter a valid Block ID (1-9) and a Color.");
+            }
+        });
+
+        document.getElementById("Reset").addEventListener("click", function () {
+            document.querySelectorAll(".grid-item").forEach(item => {
+                item.style.backgroundColor = "transparent";
+            });
+        });
